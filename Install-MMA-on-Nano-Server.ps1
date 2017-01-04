5b8ce4714588668ffb9f85d0c75cb69ba68fad61
Gregory May <mrgregmay@gmail.com>
2017-01-04 09:37:46 +0200

    Add files via upload

--- Install-MMA-on-Nano-Server.ps1
+++ Install-MMA-on-Nano-Server.ps1
@@ -6,7 +6,7 @@
      Install-MMA-on-Nano-Server.ps1 MyNanoServerName 1234567 1234567
 
     .DESCRIPTION
-     This script installs the MMA on the given Nano Server machine. The agent install file MMASetup-AMD64.exe must exist in the current path. Download from https://go.microsoft.com/fwlink/?LinkId=828603 or your OMS workspace.
+     This script installs the MMA on the specified Nano Server machine. The agent install file MMASetup-AMD64.exe must exist in the current path. Download from https://go.microsoft.com/fwlink/?LinkId=828603 or your OMS workspace.
      This script has to be run with administrative privileges. The user account which is used to connect to the Nano Server must also have administrative rights on the Nano Server.
 #>
 
@@ -15,11 +15,27 @@ param (
 [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] [string]$OPSINSIGHTS_WS_ID,
 [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] [string]$OPSINSIGHTS_WS_KEY)
 
+Write-Output "`nAdding $NanoServer to local WSMAN TrustedHosts client list"
 Set-Item wsman:localhost\Client\TrustedHosts "$NanoServer" -Concatenate –Force
+Write-Output "`nInitiating PSSession to $NanoServer"
 $TargetSession = new-PSSession -ComputerName $NanoServer -Credential (Get-Credential)
-Copy-Item -Path ".\MMASetup-AMD64.exe" -Destination "C:\" -force -ToSession $TargetSession
-New-PSSession $TargetSession
-Invoke-command -session $TargetSession -scriptblock {
-    C:\MMASetup-AMD64.exe /Q:A /R:N /C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=' + $OPSINSIGHTS_WS_ID + ' OPINSIGHTS_WORKSPACE_KEY=' + $OPSINSIGHTS_WS_KEY + ' AcceptEndUserLicenseAgreement=1" | Write-Output
-    Get-service healthservice
-    Remove-Item C:\MMASetup-AMD64.exe -Force }
+Write-Output "Copying MMASetup-AMD64.exe file to $NanoServer C:\"
+Copy-Item -Path ".\MMASetup-AMD64.exe" -Destination "C:\" -force -ToSession $TargetSession -Verbose
+If ($?) {
+    New-PSSession $TargetSession
+    Write-Output "`nExecuting remote installation with:  `n Workspace ID: $OPSINSIGHTS_WS_ID  `n Workspace Key: $OPSINSIGHTS_WS_KEY"
+    $installcmd = "C:\MMASetup-AMD64.exe"
+    $installargs = "/Q:A /R:N /C:""setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=$OPSINSIGHTS_WS_ID OPINSIGHTS_WORKSPACE_KEY=$OPSINSIGHTS_WS_KEY AcceptEndUserLicenseAgreement=1"""
+    Invoke-command -session $TargetSession -scriptblock {
+        param($Rinstallcmd, $Rinstallargs)
+        Write-Output "`n$Rinstallcmd $Rinstallargs"
+        start-process $Rinstallcmd -ArgumentList $Rinstallargs -Wait
+        Write-Output "`nWaiting 20 seconds for HealthService to bootstrap and start"
+        Start-Sleep -s 20
+        Get-Service healthservice
+        Remove-Item C:\MMASetup-AMD64.exe -Force } -ArgumentList $installcmd, $installargs
+}
+Else {
+    Write-Output "Unable to Copy File. Check Powershell version = WMF5"
+    Break
+}
